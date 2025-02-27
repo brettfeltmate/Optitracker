73e9544aa3f126480708b654904bec4a817ce576
Brett Feltmate <brettfeltmate@MacBookAir.ht.home>
2025-02-27 19:34:27 -0400

    Ensure mouse_position.csv doesn't exist at launch.

--- optitracker/OptiTracker.py
+++ optitracker/OptiTracker.py
@@ -109,6 +109,9 @@ class Optitracker(object):
             self.__mouse_thread = None
             self.__stop_mouse_thread = False
 
+            if os.path.exists(self.__data_dir):
+                os.remove(self.__data_dir)
+
             with open(self.__data_dir, 'w', newline='') as file:
                 writer = DictWriter(file, fieldnames=['frame_number', 'pos_x', 'pos_y', 'pos_z'])
                 writer.writeheader()
